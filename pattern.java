982215625b4443d40867f270df4b477a93d4331f
vikram-R23 <rmvikram05@gmail.com>
2025-07-12 14:43:58 +0530

    another new file for pattern

---
+++ pattern.java
@@ -0,0 +1,28 @@
+public class pattern {
+    public static void printDiamondPattern(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = n; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 5; // You can change n for different sizes
+        printDiamondPattern(n);
+    }
+    
+}
